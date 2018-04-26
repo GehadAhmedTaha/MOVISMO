2d0ac0d0eb3761e944a7ebf2f29eef6393e6efa8
gehadAziz <gehad.ahmed2112@gmail.com>
2018-04-26 06:26:17 +0200

    Home screen done - Download movies via Almofire API, and download movies' images via SDWebImage

--- MOVISMO/Controllers/HomePageViewController.swift
+++ MOVISMO/Controllers/HomePageViewController.swift
@@ -8,38 +8,39 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class HomePageViewController:UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-
     @IBOutlet weak var MoviesCollectionView: UICollectionView!
+    let API_KEY = "d6c15d7db1d5269f5f7973e081b8969b"
+    let API_BASE_URL = "https://api.themoviedb.org/3/discover/movie"
+    let IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w185/"
+    var moviesArray = Array<Movie>()
+    var sortType = "popularity.desc"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         MoviesCollectionView.dataSource = self
         MoviesCollectionView.delegate = self
-        
-        
-        /* let layout = self.MoviesCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
-         
-         layout.sectionInset = UIEdgeInsetsMake(0, 3, 0, 3)
-         layout.minimumInteritemSpacing = 3
-         
-         layout.itemSize = CGSize(width: (self.MoviesCollectionView.frame.width)/2, height: (self.MoviesCollectionView.frame.height)/2)*/
-        
-        fetchDataFromMovieDB()
-        
+        getMoviesviaApi() {_,_ in }
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10;
+        return moviesArray.count;
         
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = MoviesCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomePageCollectionViewCell
-        
-        cell.movieImg.image = UIImage(named: "defaultMovie")
-        cell.movieTitle.text = "Movie title"
+        var fullImageURL = IMAGE_BASE_URL + moviesArray[indexPath.row].posterPath!
+        cell.movieImg.sd_setImage(with: URL(string: fullImageURL), placeholderImage: UIImage(named: "defaultMovie"))
+
+        //cell.movieImg.sd_setImage(with: URL(string: IMAGE_BASE_URL + moviesArray[indexPath.row].posterPath! ), placeholderImage: UIImage(named: "defaultMovie"))
+       // cell.movieImg.image = UIImage(named: "defaultMovie")
+        print(indexPath.row)
+        cell.movieTitle.text = moviesArray[indexPath.row].title
         cell.layer.borderColor = UIColor.lightGray.cgColor
         cell.layer.borderWidth = 0.5
         return cell;
@@ -49,23 +50,82 @@ class HomePageViewController:UIViewController, UICollectionViewDataSource, UICol
         //
     }
     
-    func fetchDataFromMovieDB(){
-        //http://api.themoviedb.org/3/discover/movie?api_key=YOUR_KEY&sort_by=popularity.desc
-        Alamofire.request("http://api.themoviedb.org/3/discover/movie?api_key=d6c15d7db1d5269f5f7973e081b8969b&sort_by=popularity.desc").responseJSON{
-            response in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-            
-            if let json = response.result.value {
-                print("JSON: \(json)") // serialized json response
-            }
-            
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
-            }
+    func getMoviesviaApi(completion: @escaping (Bool, Error?) -> ()) {
+        
+        
+        let params: Parameters = ["api_key": API_KEY,
+                                  "sort_by" : sortType]
+        
+        Alamofire.request(API_BASE_URL, parameters: params)
+            .responseJSON { response in
+                switch response.result {
+                case .success:
+                    if let value = response.result.value {
+                        let json = JSON(value)
+                        print("JSON: \(json.count))")
+                        let json_movies = json["results"].array
+                        print("JSON Movies : \(json_movies?.count ?? 0)")
+                        if let movies = json_movies {
+                            self.parseMoviesJsonArray(movies)
+                            completion(true, nil)
+                            
+                        } else  {
+                            
+                            completion(false, NSError(domain: "MoviesListNotFound", code: 200, userInfo: nil))
+                            
+                        }
+                       // self.activityIndicatorView.stopAnimating()
+                        
+                        
+                    }
+                case .failure(let error):
+                    print(error)
+                    completion(false, error)
+                   // self.activityIndicatorView.stopAnimating()
+                }
+                
         }
     }
+    func parseMoviesJsonArray(_ data : Array<JSON>){
+        var tempMovie : Movie
+        for movieJson in data {
+            tempMovie = Movie()
+            tempMovie.movieID = movieJson["id"].intValue
+            tempMovie.title = movieJson["title"].stringValue
+            tempMovie.overview = movieJson["overview"].stringValue
+            tempMovie.posterPath = movieJson["poster_path"].stringValue
+            tempMovie.rate = movieJson["vote_average"].intValue
+            tempMovie.releaseDate = getFormattedDate(movieJson["release_date"].stringValue)
+            moviesArray.append(tempMovie)
+        }
+        self.MoviesCollectionView.reloadData()
+    }
+    
+    // MARK: - Utilities
+    func getFormattedDate (_ releaseDate : String) -> Date? {
+        
+        //print("\(releaseDate)")
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: releaseDate)
+        
+        
+        
+    }
+    
+    func getFormattedDateForUI(_ date: Date?) -> String {
+        
+        if let release_date = date {
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            return formatter.string(from: release_date)
+        }
+        
+        return ""
+    }
+
     /*
     // MARK: - Navigation
 
--- MOVISMO/Models/Movie.swift
+++ MOVISMO/Models/Movie.swift
@@ -8,5 +8,26 @@
 
 import Foundation
 class Movie{
-    
+    var movieID : Int?
+    var rate : Int?
+    var title : String?
+    var overview : String?
+    var posterPath : String?
+    var releaseDate : Date?
+    init() {
+        self.movieID = Int()
+        self.rate = Int()
+        self.title = String()
+        self.overview = String()
+        self.posterPath = String()
+        self.releaseDate = Date()
+    }
+    init(movieID: Int?, rate: Int?, title: String?, overview: String?, posterPath: String?, releaseDate : Date?) {
+     self.movieID = movieID
+     self.rate = rate
+     self.title = title
+     self.posterPath = posterPath
+     self.overview = overview
+     self.releaseDate = releaseDate
+     }
 }
